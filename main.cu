a70129264501ff7f41e505b646cf1b3d77a2ede7
Sterbic <luka.sterbic@fer.hr>
2012-11-08 15:48:44 +0100

    dodan ignore

--- main.cu
+++ main.cu
@@ -25,64 +25,64 @@ __global__ void kernel() {
 }
 
 int main(int argc, char *argv[]) {
-	printf("> Welcome to SWaligner v%s\n\n", VERSION);
+    printf("> Welcome to SWaligner v%s\n\n", VERSION);
 
-	if (argc != 7) {
+    if (argc != 7) {
         printf("Expected 6 input arguments, not %d!\n\n", argc - 1);
         return -1;
     }
 
-	FASTAsequence first(argv[1]);
-	FASTAsequence second(argv[2]);
+    FASTAsequence first(argv[1]);
+    FASTAsequence second(argv[2]);
 
-	printf("Loading input sequences... ");
-	if(!first.load() || !second.load()) {
-		printf("ERROR\nAn error has occured while loading input sequences!\n\n");
-		exit(-1);
-	}
-	else {
-		printf("DONE\n\n");
-	}
+    printf("Loading input sequences... ");
+    if(!first.load() || !second.load()) {
+	printf("ERROR\nAn error has occured while loading input sequences!\n\n");
+	exit(-1);
+    }
+    else {
+	printf("DONE\n\n");
+    }
+    
+    printf("First sequence:\n%s\n\n", first.getSequenceName());
+    printf("Second sequence:\n%s\n\n", second.getSequenceName());
 
-	printf("First sequence:\n%s\n\n", first.getSequenceName());
-	printf("Second sequence:\n%s\n\n", second.getSequenceName());
-
-	scoring values;
-	printf("Initializing scoring values... ");
+    scoring values;
+    printf("Initializing scoring values... ");
     values.match = atoi(argv[3]);
     values.mismatch = atoi(argv[4]);
     values.insertion = atoi(argv[5]);
     values.deletion = atoi(argv[6]);
 
-	if(values.match < 1 || values.mismatch > -1 || values.insertion > -1 || values.deletion > -1) {
-		printf("ERROR\nOne or more scoring values were not usable!\n\n");
-		exit(-1);
-	}
-	else {
-		printf("DONE\n\nScoring values:\n");
-		printf("	>Match: %d\n", values.match);
-		printf("	>Mismatch: %d\n", values.mismatch);
-		printf("	>Insertion: %d\n", values.insertion);
-		printf("	>Deletion: %d\n\n", values.deletion);
-	}
+    if(values.match < 1 || values.mismatch > -1 || values.insertion > -1 || values.deletion > -1) {
+	printf("ERROR\nOne or more scoring values were not usable!\n\n");
+	exit(-1);
+    }
+    else {
+	printf("DONE\n\nScoring values:\n");
+	printf("	>Match: %d\n", values.match);
+	printf("	>Mismatch: %d\n", values.mismatch);
+	printf("	>Insertion: %d\n", values.insertion);
+	printf("	>Deletion: %d\n\n", values.deletion);
+    }
 
-	printf("Starting alignment process... ");
+    printf("Starting alignment process... ");
 
-	cudaEvent_t start, stop;
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop);
-	cudaEventRecord(start, 0);
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start, 0);
 
-	kernel<<<10, 10>>>();
+    kernel<<<10, 10>>>();
+    
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    
+    printf("DONE\n\n");
 
-	cudaEventRecord(stop, 0);
-	cudaEventSynchronize(stop);
-
-	printf("DONE\n\n");
-
-	float time;
-	cudaEventElapsedTime(&time, start, stop);
-	printf("Kernel executed in %f s\n", time / 1000);
+    float time;
+    cudaEventElapsedTime(&time, start, stop);
+    printf("Kernel executed in %f s\n", time / 1000);
 
     return 0;
 }
