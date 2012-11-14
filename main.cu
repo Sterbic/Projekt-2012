6af96a5d207c862d2048d8f2a614ce3af4d61c9a
Luka Sterbic <luka.sterbic@fer.hr>
2012-11-14 16:36:40 +0100

    radi za T=9

--- main.cu
+++ main.cu
@@ -33,65 +33,70 @@ typedef struct {
 __global__ void shortPhase(int dk, element *matrix, char *first,
 	int firstLength, char *second, int secondLength, scoring values,
 	alignmentScore *blockScore) {
-	printf("string %d %s\n", firstLength, first);
-    printf("in short phase thread %d\n", threadIdx.x);
-    if(threadIdx.x == 0) {
-    //	printf("got arguments:\n dk: %d\nfirst seq of length %d: %s\n", dk, firstLength, first);
-    //	printf("second seq of length %d: %s\n", secondLength, second);
-    //	printf("scoring: %d, %d, %d, %d\n", values.match, values.mismatch, values.first, values.extension);
-    	//printf("blockScore 0 score: %d\n", blockScore[0].score);
-    //	printf("elements matrix: [0,0] = %d, [2,2] = %d, [5,7] = %d\n", matrix[0].H,
-    	//		matrix[3 + (firstLength + 1) * 2].H, matrix[7 + (firstLength + 1) * 5].H);
-    }
-
 	__shared__ alignmentScore blockMax[THREADS_PER_BLOCK];
 	blockMax[threadIdx.x].score = 0;
 
 	int i = ALPHA * (blockIdx.x * THREADS_PER_BLOCK + threadIdx.x);
 	int j = secondLength / BLOCKS_PER_GRID * (dk - blockIdx.x) - threadIdx.x;
-	printf("blockIdx.x = %d, threadIdx.x = %d, i = %d, j = %d\n", blockIdx.x, threadIdx.x, i, j);
 
 	if(j < 0) {
 		i -= ALPHA * BLOCKS_PER_GRID * THREADS_PER_BLOCK;
-		j = secondLength - j;
+		j = secondLength + j;
 	}
 
+//	printf("blockIdx.x = %d, threadIdx.x = %d, i = %d, j = %d\n", blockIdx.x, threadIdx.x, i, j);
+	int cols = secondLength + 1;
+	element *test = &matrix[6 + 6 * cols];
+
 	for(int innerDiagonal = 0; innerDiagonal < THREADS_PER_BLOCK; innerDiagonal++) {
+		if(j == secondLength) {
+					j = 0;
+					i += BLOCKS_PER_GRID * ALPHA * THREADS_PER_BLOCK;
+		}
+
 		if(i >= 0 && i < firstLength) {
-			for(int a = 0; a < ALPHA; a++) {
-				element *current = matrix + j + 1 + (i + a + 1) * (secondLength + 1);
-				element *left = matrix + j + (i + a + 1) * (secondLength + 1);
-				element *up = matrix + j + 1 + (i + a) * (secondLength + 1);
-				element *diagonal = matrix + j + (i + a) * (secondLength + 1);
+			element *current = &matrix[j + 1 + (i + 1) * cols];
+			element *left = current - 1;
+			element *up = current - cols;
+			element *diagonal = up - 1;
 
-				int matchMissmatch = values.mismatch;
-				if(first[i + a] == second[j]) matchMissmatch = values.match;
+			printf("[%d, %d] = [%d, %d, %d] -- old\n", i, j, current->H, current->E, current->F);
+			if(current == test) printf("Pogodak u shortu!!!\n");
 
-				current->E = max(left->E + values.extension, left->H + values.first);
-				current->F = max(up->F + values.extension, up->H + values.first);
-				current->H = max(max(0, current->E), max(current->F, diagonal->H + matchMissmatch));
+			int matchMissmatch = values.mismatch;
+			if(first[i] == second[j])
+				matchMissmatch = values.match;
 
-				if(current->H > blockMax[threadIdx.x].score) {
-					blockMax[threadIdx.x].score = current->H;
-					blockMax[threadIdx.x].column = j + 1;
-					blockMax[threadIdx.x].row = i + 1;
-				}
+			current->E = max(left->E + values.extension, left->H + values.first);
+			current->F = max(up->F + values.extension, up->H + values.first);
+			current->H = max(max(0, current->E), max(current->F, diagonal->H + matchMissmatch));
+
+		//	printf("[%d, %d] = [%d, %d, %d] -- new\n\n", i, j, current->H, current->E, current->F);
+
+			if(current->H > blockMax[threadIdx.x].score) {
+				blockMax[threadIdx.x].score = current->H;
+				blockMax[threadIdx.x].row = i;
+				blockMax[threadIdx.x].column = j;
 			}
 		}
 
 		j++;
 
-		if(j == secondLength) {
+/*		if(j == secondLength) {
 			j = 0;
 			i += BLOCKS_PER_GRID * ALPHA * THREADS_PER_BLOCK;
-		}
+		} */
 
 		__syncthreads();
 	}
 
-	int index = blockDim.x / 2;
-	while(i != 0) {
-		if(threadIdx.x < index) {
+	int nearestPowof2 = 1;
+	while (nearestPowof2 < THREADS_PER_BLOCK)
+		nearestPowof2 <<= 1;
+
+	int index = nearestPowof2 / 2;
+	while(index != 0) {
+		if(threadIdx.x < index && threadIdx.x + index < THREADS_PER_BLOCK) {
 			if(blockMax[threadIdx.x].score < blockMax[threadIdx.x + index].score)
 				blockMax[threadIdx.x] = blockMax[threadIdx.x + index];
 		}
@@ -100,8 +105,11 @@ __global__ void shortPhase(int dk, element *matrix, char *first,
 		index /= 2;
 	}
 
-	if(threadIdx.x == 0) {
+	if(threadIdx.x == 0 && blockScore[blockIdx.x].score < blockMax[0].score) {
 		blockScore[blockIdx.x] = blockMax[0];
+		for(int i = 0; i < THREADS_PER_BLOCK; i++) {
+			printf("%d ", blockMax[i].score);
+		}
 	}
 }
 
@@ -112,27 +120,34 @@ __global__ void longPhase(int dk, element *matrix,char *first,
 	blockMax[threadIdx.x].score = 0;
 
 	int i = ALPHA * (blockIdx.x * THREADS_PER_BLOCK + threadIdx.x);
-	int j = secondLength / BLOCKS_PER_GRID * (dk - blockIdx.x) - threadIdx.x;
+	int j = secondLength / BLOCKS_PER_GRID * (dk - blockIdx.x) - threadIdx.x + THREADS_PER_BLOCK;
 
-	for(int innerDiagonal = THREADS_PER_BLOCK; innerDiagonal < secondLength / BLOCKS_PER_GRID; innerDiagonal++) {
+	int cols = secondLength + 1;
+	element *test = &matrix[6 + 6 * cols];
+
+	for(int innerDiagonal = THREADS_PER_BLOCK;
+			innerDiagonal < secondLength / BLOCKS_PER_GRID; innerDiagonal++) {
 		if(i >= 0 && i < firstLength) {
 			for(int a = 0; a < ALPHA; a++) {
-				element *current = matrix + j + 1 + (i + a + 1) * (secondLength + 1);
-				element *left = matrix + j + (i + a + 1) * (secondLength + 1);
-				element *up = matrix + j + 1 + (i + a) * (secondLength + 1);
-				element *diagonal = matrix + j + (i + a) * (secondLength + 1);
+				element *current = &matrix[j + 1 + (i + 1) * cols];
+				element *left = current - 1;
+				element *up = current - cols;
+				element *diagonal = up - 1;
+				if(current == test) printf("Pogodak u longu!!!\n");
 
 				int matchMissmatch = values.mismatch;
-				if(first[i + a] == second[j]) matchMissmatch = values.match;
+				if(first[i] == second[j])
+				matchMissmatch = values.match;
 
 				current->E = max(left->E + values.extension, left->H + values.first);
 				current->F = max(up->F + values.extension, up->H + values.first);
 				current->H = max(max(0, current->E), max(current->F, diagonal->H + matchMissmatch));
+				printf("i = %d, j = %d, H = %d\n", i, j, current->H);
 
 				if(current->H > blockMax[threadIdx.x].score) {
 					blockMax[threadIdx.x].score = current->H;
-					blockMax[threadIdx.x].column = j + 1;
-					blockMax[threadIdx.x].row = i + 1;
+					blockMax[threadIdx.x].row = i;
+					blockMax[threadIdx.x].column = j;
 				}
 			}
 		}
@@ -142,9 +157,13 @@ __global__ void longPhase(int dk, element *matrix,char *first,
 		__syncthreads();
 	}
 
-	int index = blockDim.x / 2;
-	while(i != 0) {
-		if(threadIdx.x < index) {
+	int nearestPowof2 = 1;
+	while (nearestPowof2 < THREADS_PER_BLOCK)
+		nearestPowof2 <<= 1;
+
+	int index = nearestPowof2 / 2;
+	while(index != 0) {
+		if(threadIdx.x < index && threadIdx.x + index < THREADS_PER_BLOCK) {
 			if(blockMax[threadIdx.x].score < blockMax[threadIdx.x + index].score)
 				blockMax[threadIdx.x] = blockMax[threadIdx.x + index];
 		}
@@ -153,9 +172,15 @@ __global__ void longPhase(int dk, element *matrix,char *first,
 		index /= 2;
 	}
 
-	if(threadIdx.x == 0) {
+	if(threadIdx.x == 0 && blockScore[blockIdx.x].score < blockMax[0].score) {
 		blockScore[blockIdx.x] = blockMax[0];
 	}
+}
+
+void exitWithMsg(char *msg, int exitCode) {
+	printf("ERROR\n");
+	printf("%s\n\n", msg);
+	exit(exitCode);
 }
 
 int main(int argc, char *argv[]) {
@@ -170,13 +195,10 @@ int main(int argc, char *argv[]) {
     FASTAsequence second(argv[2]);
 
     printf("Loading input sequences... ");
-    if(!first.load() || !second.load()) {
-
-      printf("ERROR\nAn error has occured while loading input sequences!\n\n");
-      exit(-1);
-    }
+    if(!first.load() || !second.load())
+    	exitWithMsg("An error has occured while loading input sequences!", -1);
     else
-      printf("DONE\n\n");
+    	printf("DONE\n\n");
 	
     printf("First sequence of length %d:\n%s\n\n", first.getLength(), first.getSequenceName());
     printf("Second sequence of length %d:\n%s\n\n", second.getLength(), second.getSequenceName());
@@ -188,10 +210,8 @@ int main(int argc, char *argv[]) {
     values.first = atoi(argv[5]);
     values.extension = atoi(argv[6]);
 
-	if(values.match < 1 || values.mismatch > -1 || values.first > -1 || values.extension > -1) {
-		printf("ERROR\nOne or more scoring values were not usable!\n\n");
-		exit(-1);
-	}
+	if(values.match < 1 || values.mismatch > -1 || values.first > -1 || values.extension > -1)
+		exitWithMsg("One or more scoring values were not usable!", -1);
 	else {
 		printf("DONE\n\nScoring values:\n");
 		printf("	>Match: %d\n", values.match);
@@ -202,29 +222,45 @@ int main(int argc, char *argv[]) {
 
     printf("Starting alignment process... ");
 
+    cudaError_t errAlloc, errCpy, errMemset;
+
     element *matrix;
     int matrixSize = sizeof(element) * (first.getLength() + 1) * (second.getLength() + 1);
-    cudaMalloc(&matrix, matrixSize);
-    cudaMemset(matrix, 0, matrixSize);
+    errAlloc = cudaMalloc(&matrix, matrixSize);
+    errMemset = cudaMemset(matrix, 0, matrixSize);
+    if(errAlloc != 0 || errMemset != 0)
+    	exitWithMsg("An error has occured while creating alignment matrix on device.", -1);
 
     alignmentScore max;
     max.score = 0;
+    max.row = 0;
+    max.column = 0;
 
     alignmentScore *blockScores;
     int blockScoreSize = sizeof(alignmentScore) * BLOCKS_PER_GRID;
     blockScores = (alignmentScore *)malloc(blockScoreSize);
+    if(blockScores == NULL)
+    	exitWithMsg("An error has occured while allocating blockScores array on host.", -1);
     
 	alignmentScore *devBlockScore;
-    cudaMalloc(&devBlockScore, blockScoreSize);
-    cudaMemset(devBlockScore, 0, blockScoreSize);
+    errAlloc = cudaMalloc(&devBlockScore, blockScoreSize);
+    errMemset = cudaMemset(devBlockScore, 0, blockScoreSize);
+    if(errAlloc != 0 || errMemset != 0)
+    	exitWithMsg("An error has occured while creating blockScore array on device.", -1);
 
     char *devFirst;
-    cudaMalloc(&devFirst, first.getLength() * sizeof(char));
-    cudaMemcpy(devFirst, first.getSequence(), first.getLength() * sizeof(char), cudaMemcpyHostToDevice);
+    errAlloc = cudaMalloc(&devFirst, first.getLength() * sizeof(char));
+    errCpy = cudaMemcpy(devFirst, first.getSequence(),
+    		first.getLength() * sizeof(char), cudaMemcpyHostToDevice);
+    if(errAlloc != 0 || errCpy != 0)
+    	exitWithMsg("An error has occured while transfering first sequence to device.", -1);
 
     char *devSecond;
-    cudaMalloc(&devSecond, second.getLength() * sizeof(char));
-    cudaMemcpy(devSecond, second.getSequence(), second.getLength() * sizeof(char), cudaMemcpyHostToDevice);
+    errAlloc = cudaMalloc(&devSecond, second.getLength() * sizeof(char));
+    errCpy = cudaMemcpy(devSecond, second.getSequence(),
+    		second.getLength() * sizeof(char), cudaMemcpyHostToDevice);
+    if(errAlloc != 0 || errCpy != 0)
+    	exitWithMsg("An error has occured while transfering second sequence to device.", -1);
 
 	int D = BLOCKS_PER_GRID + first.getLength() / (ALPHA * THREADS_PER_BLOCK) - 1;
 	
@@ -232,8 +268,7 @@ int main(int argc, char *argv[]) {
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
-    
-    printf("> %s\n> %s\n", first.getSequence(), second.getSequence());
+    //D + BLOCKS_PER_GRID
     for(int dk = 0; dk < D + BLOCKS_PER_GRID; dk++) {
     	shortPhase<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK>>>(
     			dk,
@@ -247,7 +282,8 @@ int main(int argc, char *argv[]) {
     			);
 
         printf("\nLaunch %d, error: %s\n", dk, cudaGetErrorString(cudaDeviceSynchronize()));
-    	
+
+        if(dk < D + BLOCKS_PER_GRID - 1) {
     	longPhase<<<BLOCKS_PER_GRID, THREADS_PER_BLOCK>>>(
     			dk,
     			matrix,
@@ -258,21 +294,34 @@ int main(int argc, char *argv[]) {
     			values,
     			devBlockScore
     			);
-
-    	cudaMemcpy(blockScores, devBlockScore, blockScoreSize, cudaMemcpyDeviceToHost);
-    	for(int i = 0; i < BLOCKS_PER_GRID; i++) {
-    		if(max.score < blockScores[i].score) {
-    			max.score = blockScores[i].score;
-    			max.column = blockScores[i].column;
-    			max.row = blockScores[i].row;
-    		}
-    	}
+        }
     }
     
+    cudaMemcpy(blockScores, devBlockScore, blockScoreSize, cudaMemcpyDeviceToHost);
+	for(int i = 0; i < BLOCKS_PER_GRID; i++) {
+		if(max.score < blockScores[i].score) {
+			max.score = blockScores[i].score;
+			max.column = blockScores[i].column;
+			max.row = blockScores[i].row;
+		}
+    }
+
     cudaEventRecord(stop, 0);
     cudaEventSynchronize(stop);
     
     printf("DONE\n\n");
+
+
+    element *hostMatrix = (element *)malloc(matrixSize);
+    cudaMemcpy(hostMatrix, matrix, matrixSize, cudaMemcpyDeviceToHost);
+    for(int i = 0; i <= first.getLength(); i++) {
+    	for(int j = 0; j <= second.getLength(); j++) {
+    		printf("%d ", hostMatrix[j + i * (second.getLength() + 1)].H);
+    	}
+    	printf("\n");
+    }
+    free(hostMatrix);
+
 
     float time;
     cudaEventElapsedTime(&time, start, stop);
