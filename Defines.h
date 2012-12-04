b48f044aa474826b6850f379f366a2af31b623d1
Luka Sterbic <luka.sterbic@fer.hr>
2012-12-04 16:36:26 +0100

    globalne definicije u svoj header

---
+++ Defines.h
@@ -0,0 +1,14 @@
+/*
+ * Defines.h
+ *
+ *  Created on: Dec 4, 2012
+ *      Author: Luka
+ */
+
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+#define VERSION "0.4.0"
+#define ALPHA 4
+
+#endif /* DEFINES_H_ */
