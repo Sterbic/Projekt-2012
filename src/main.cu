6d1355e11bcda98e8c1acddce0d7d6a171424a0d
Luka Sterbic <luka.sterbic@fer.hr>
2013-01-16 19:10:37 +0100

    S2

--- src/main.cu
+++ src/main.cu
@@ -219,7 +219,7 @@ int main(int argc, char *argv[]) {
 		fread(specialRow, sizeof(int2), query.getSecond()->getPaddedLength(), f);
 		fclose(f);
 
-		int getVertical = min(chunkSize, maxTrace.row - specialRowIndex);
+		int getVertical = min(chunkSize, maxTrace.row - specialRowIndex + 1);
 		safeAPIcall(cudaMemcpy(devColumn, firstReversed + heightOffset,
 				getVertical * sizeof(char), cudaMemcpyHostToDevice), __LINE__);
 
